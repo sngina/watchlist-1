0e9127f571c341d988479976dcf8525ae7808a44
sngina <sngina707@gmail.com>
2021-06-09 17:25:51 +0300

    updated user photo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 export MOVIE_API_KEY='a14e8e63329c74896bae187a6c0e2489'
 export SECRET_KEY='1tsImp0ssiblet0hacketh1s0ne0ut'
-export MAIL_USERNAME=<Your Email Address>
-export MAIL_PASSWORD=<Your Email Password>
+# export MAIL_USERNAME= 'yehiji4365@relumyx.com'
+# export MAIL_PASSWORD= '12345678'
 python3 manage.py server
